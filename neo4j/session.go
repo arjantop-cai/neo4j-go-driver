e96a8743bdd8582e03901e77274414eabbfbd297
Arjan Topolovec <arjan.topolovec@cookie.ai>
2021-07-27 10:56:43 +0200

    wip

--- neo4j/session.go
+++ neo4j/session.go
@@ -420,8 +420,9 @@ func (s *session) Run(
 		}
 		err = conn.ForceReset()
 		if err == nil {
-			s.log.Error(log.Session, s.logId, err)
 			break
+		} else {
+			s.log.Error(log.Session, s.logId, err)
 		}
 	}
 
